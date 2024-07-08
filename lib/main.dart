c8388b71271a57aaa794eba1c9e375655edb466c
alvinpurwo03 <alvinpurwo03@gmail.com>
2024-07-08 02:29:09 +0000

    belajar flutter

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('BELAJAR FLUTTER'),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Center(
+          child: Text(
+            'Hello Dunia',
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
